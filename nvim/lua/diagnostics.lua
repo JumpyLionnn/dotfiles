d0082791607327fd6a77a9c8709d528f5d6b2623
jumpylionnn <jumpylionn1@gmail.com>
2024-07-06 15:09:38 +0300

    Disable nvim info and hint diagnostic signs

--- nvim/lua/diagnostics.lua
+++ nvim/lua/diagnostics.lua
@@ -5,10 +5,14 @@ vim.diagnostic.config({
         text = {
             [vim.diagnostic.severity.ERROR] = '',
             [vim.diagnostic.severity.WARN] = '',
+            [vim.diagnostic.severity.INFO] = '',
+            [vim.diagnostic.severity.HINT] = '',
         },
         linehl = {
             [vim.diagnostic.severity.ERROR] = '',
             [vim.diagnostic.severity.WARN] = '',
+            [vim.diagnostic.severity.INFO] = '',
+            [vim.diagnostic.severity.HINT] = '',
         },
         numhl = {
             [vim.diagnostic.severity.ERROR] = 'DiagnosticError',
