ff816e5ce27dbeb49d2ea5ff05330a2092121f84
jumpylionnn <jumpylionn1@gmail.com>
2024-08-02 16:23:31 +0300

    Fix yazi in nvim q for exit

--- nvim/lua/plugins/yazi.lua
+++ nvim/lua/plugins/yazi.lua
@@ -3,7 +3,23 @@ vim.api.nvim_create_autocmd('UiEnter', {
     require('yazi').setup({
       open_for_directories = true,
       floating_window_scaling_factor = 1.0,
-      yazi_floating_window_border = 'none'
+      yazi_floating_window_border = 'none',
+        hooks = {
+            yazi_closed_successfully = function(chosen_file, _, _)
+                local bufs = vim.api.nvim_list_bufs();
+                local buffers_empty = true;
+                for _, buf in ipairs(bufs) do
+                    local lines = vim.api.nvim_buf_get_lines(buf, 0, -1, false);
+                    if #lines > 1 or lines[1] ~= "" then
+                        buffers_empty = false;
+                        break;
+                    end
+                end
+                if chosen_file == nil and buffers_empty then
+                    vim.cmd("qa")
+                end
+            end
+        }
     })
     vim.keymap.set('n', '<leader>pv', require('yazi').yazi)
   end,
